344e9dbf22180f051c8776cceb696dc6138c0dae
Vrush2004 <vrushalichaube@gmail.com>
2024-09-16 10:56:07 +0530

    product controller

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,59 @@
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+// @desc    Fetch all products
+// @api     GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
+
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
+    .sort({ updatedAt: "desc" });
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
+});
+
+// @desc    Fetch single product
+// @api     GET /api/products/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+// @desc    Delete a product
+// @api     DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export {
+    getProducts,
+    getProductById,
+    deleteProduct,
+}
