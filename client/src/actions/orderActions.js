9a75c749435538c309cf9a86f36a0585b56847b8
Vrush2004 <vrushalichaube@gmail.com>
2024-09-17 14:03:10 +0530

    order action

---
+++ client/src/actions/orderActions.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+
+import {
+  orderCreateActions,
+  orderDeliverActions,
+  orderDetailsActions,
+  orderListActions,
+  orderListMyActions,
+  orderPayActions,
+} from "../reducers/orderReducers";
+
+export const createOrder = (order) => async (dispatch, getState) => {
+  try {
+    dispatch(orderCreateActions.orderCreateRequest());
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.post(`/api/orders`, order, config);
+
+    dispatch(orderCreateActions.orderCreateSuccess(data));
+  } catch (error) {
+    dispatch(
+      orderCreateActions.orderCreateFail(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      )
+    );
+  }
+};
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch(orderDetailsActions.orderDetailsRequest());
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/orders/${id}`, config);
+
+    dispatch(orderDetailsActions.orderDetailsSuccess(data));
+  } catch (error) {
+    dispatch(
+      orderDetailsActions.orderDetailsFail(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      )
+    );
+  }
+};
