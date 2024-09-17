b418523b271f18535af6cd05b05a0f508ff10a14
Vrush2004 <vrushalichaube@gmail.com>
2024-09-17 14:04:03 +0530

    order action

--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -65,3 +65,69 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
     );
   }
 };
+
+export const payOrder =
+  (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+      dispatch(orderPayActions.orderPayRequest());
+
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.put(
+        `/api/orders/${orderId}/pay`,
+        paymentResult,
+        config
+      );
+
+      dispatch(orderPayActions.orderPaySuccess(data));
+    } catch (error) {
+      dispatch(
+        orderPayActions.orderPayFail(
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+        )
+      );
+    }
+  };
+
+export const deliverOrder = (order) => async (dispatch, getState) => {
+  try {
+    dispatch(orderDeliverActions.orderDeliverRequest());
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      `/api/orders/${order._id}/deliver`,
+      {},
+      config
+    );
+
+    dispatch(orderDeliverActions.orderDeliverSuccess(data));
+  } catch (error) {
+    dispatch(
+      orderDeliverActions.orderDeliverFail(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      )
+    );
+  }
+};
