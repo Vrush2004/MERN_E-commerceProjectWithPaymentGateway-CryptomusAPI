ec0fe6ed083a672473f90f7d4847f06509c71643
Vrush2004 <vrushalichaube@gmail.com>
2024-09-17 14:10:47 +0530

    user action

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -167,3 +167,61 @@ export const register = (name, email, password) => async (dispatch) => {
     }
   };
   
+  export const deleteUser = (id) => async (dispatch, getState) => {
+    try {
+      dispatch(userDeleteActions.userDeleteRequest());
+  
+      const {
+        userLogin: { userInfo },
+      } = getState();
+  
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+  
+      await axios.delete(`/api/users/${id}`, config);
+  
+      dispatch(userDeleteActions.userDeleteSuccess());
+    } catch (error) {
+      dispatch(
+        userDeleteActions.userDeleteFail(
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+        )
+      );
+    }
+  };
+  
+  export const updateUser = (user) => async (dispatch, getState) => {
+    try {
+      dispatch(userUpdateActions.userUpdateRequest());
+  
+      const {
+        userLogin: { userInfo },
+      } = getState();
+  
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+  
+      const { data } = await axios.put(`/api/users/${user._id}`, user, config);
+  
+      dispatch(userUpdateActions.userUpdateSuccess());
+      dispatch(userDetailsActions.userDetailsSuccess(data));
+    } catch (error) {
+      dispatch(
+        userUpdateActions.userUpdateFail(
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+        )
+      );
+    }
+  };
