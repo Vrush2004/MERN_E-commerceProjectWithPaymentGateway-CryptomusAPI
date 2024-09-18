70ee8915a3bbb03e580ece205f1bdc0c532fa02e
Vrush2004 <vrushalichaube@gmail.com>
2024-09-18 17:29:25 +0530

    order reducer

--- client/src/reducers/orderReducers.js
+++ client/src/reducers/orderReducers.js
@@ -66,3 +66,91 @@ const orderPaySlice = createSlice({
 
 // ORDER DELIVER SLICE
 const orderDeliverInitialState = {};
+
+const orderDeliverSlice = createSlice({
+    name: "orderDeliver",
+    initialState: orderDeliverInitialState,
+    reducers: {
+      orderDeliverRequest(state, action) {
+        return { loading: true };
+      },
+      orderDeliverSuccess(state, action) {
+        return { loading: false, success: true };
+      },
+      orderDeliverFail(state, action) {
+        return { loading: false, error: action.payload };
+      },
+      orderDeliverReset(state, action) {
+        return {};
+      },
+    },
+  });
+  
+  // ORDER LIST MY SLICE
+  const orderListMyInitialState = { orders: [] };
+  
+  const orderListMySlice = createSlice({
+    name: "orderListMy",
+    initialState: orderListMyInitialState,
+    reducers: {
+      orderListMyRequest(state, action) {
+        return { loading: true };
+      },
+      orderListMySuccess(state, action) {
+        return { loading: false, orders: action.payload };
+      },
+      orderListMyFail(state, action) {
+        return { loading: false, error: action.payload };
+      },
+      orderListMyReset(state, action) {
+        return { orders: [] };
+      },
+    },
+  });
+  
+  // ORDER LIST SLICE
+  const orderListInitialState = { orders: [] };
+  
+  const orderListSlice = createSlice({
+    name: "orderList",
+    initialState: orderListInitialState,
+    reducers: {
+      orderListRequest(state, action) {
+        return { loading: true };
+      },
+      orderListSuccess(state, action) {
+        return { loading: false, orders: action.payload };
+      },
+      orderListFail(state, action) {
+        return { loading: false, error: action.payload };
+      },
+    },
+  });
+  
+  const orderCreateActions = orderCreateSlice.actions;
+  const orderCreateReducer = orderCreateSlice.reducer;
+  const orderDetailsActions = orderDetailsSlice.actions;
+  const orderDetailsReducer = orderDetailsSlice.reducer;
+  const orderPayActions = orderPaySlice.actions;
+  const orderPayReducer = orderPaySlice.reducer;
+  const orderListMyActions = orderListMySlice.actions;
+  const orderListMyReducer = orderListMySlice.reducer;
+  const orderListActions = orderListSlice.actions;
+  const orderListReducer = orderListSlice.reducer;
+  const orderDeliverActions = orderDeliverSlice.actions;
+  const orderDeliverReducer = orderDeliverSlice.reducer;
+  
+  export {
+    orderCreateActions,
+    orderCreateReducer,
+    orderDetailsActions,
+    orderDetailsReducer,
+    orderPayActions,
+    orderPayReducer,
+    orderListMyActions,
+    orderListMyReducer,
+    orderListActions,
+    orderListReducer,
+    orderDeliverActions,
+    orderDeliverReducer,
+  };
