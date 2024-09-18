236677d4b66d879ae02db50e44b914d4fa6aa8e1
Vrush2004 <vrushalichaube@gmail.com>
2024-09-18 17:28:53 +0530

    order reducer

---
+++ client/src/reducers/orderReducers.js
@@ -0,0 +1,68 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// ORDER CREATE SLICE
+const orderCreateInitialState = {};
+
+const orderCreateSlice = createSlice({
+  name: "orderCreate",
+  initialState: orderCreateInitialState,
+  reducers: {
+    orderCreateRequest(state, action) {
+      return { loading: true };
+    },
+    orderCreateSuccess(state, action) {
+      return { loading: false, success: true, order: action.payload };
+    },
+    orderCreateFail(state, action) {
+      return { loading: false, error: action.payload };
+    },
+  },
+});
+
+// ORDER DETAILS SLICE
+const orderDetailsInitialState = {
+  loading: true,
+  orderItems: [],
+  shippingAddress: {},
+};
+
+const orderDetailsSlice = createSlice({
+  name: "orderDetails",
+  initialState: orderDetailsInitialState,
+  reducers: {
+    orderDetailsRequest(state, action) {
+      return { ...state, loading: true };
+    },
+    orderDetailsSuccess(state, action) {
+      return { loading: false, order: action.payload };
+    },
+    orderDetailsFail(state, action) {
+      return { loading: false, error: action.payload };
+    },
+  },
+});
+
+// ORDER PAY SLICE
+const orderPayInitialState = {};
+
+const orderPaySlice = createSlice({
+  name: "orderPay",
+  initialState: orderPayInitialState,
+  reducers: {
+    orderPayRequest(state, action) {
+      return { loading: true };
+    },
+    orderPaySuccess(state, action) {
+      return { loading: false, success: true };
+    },
+    orderPayFail(state, action) {
+      return { loading: false, error: action.payload };
+    },
+    orderPayReset(state, action) {
+      return {};
+    },
+  },
+});
+
+// ORDER DELIVER SLICE
+const orderDeliverInitialState = {};
