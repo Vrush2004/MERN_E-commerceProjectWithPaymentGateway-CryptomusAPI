bf2fbd158a44f62dafe1b87516d8d85853d762fa
Vrush2004 <vrushalichaube@gmail.com>
2024-09-21 10:42:24 +0530

    profile

--- client/src/pages/profile/ProfileScreen.jsx
+++ client/src/pages/profile/ProfileScreen.jsx
@@ -74,3 +74,63 @@ const ProfileScreen = () => {
       );
     }
   };
+
+  return (
+    <Layout>
+      <Header className="justify-end">
+        <div className="flex items-center divide-x divide-gray-200 border-x border-b border-gray-200">
+          <Cart className="hidden lg:block p-6 text-palette-graniteGray" />
+          <UserProfileButton className="hidden lg:block p-6 text-palette-graniteGray" />
+        </div>
+      </Header>
+      <section className="flex flex-col p-5 lg:flex-row lg:gap-x-7">
+        <div className="w-full lg:w-3/12">
+          <h2 className="text-3xl mb-6">User Profile</h2>
+          <div className="flex flex-col space-y-2">
+            {message && <Alert variant="error">{message}</Alert>}
+            {error && <Alert variant="error">{error}</Alert>}
+            {success && <Alert variant="success">Profile Updated</Alert>}
+          </div>
+          <form onSubmit={submitHandler}>
+            <div className="form-control">
+              <label className="label" htmlFor="name">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                className="input input-bordered"
+                id="name"
+                type="text"
+                placeholder="Enter your name"
+                value={inputValues.name}
+                name="name"
+                onChange={inputChangeHandler}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label" htmlFor="email">
+                <span className="label-text">Email Address</span>
+              </label>
+              <input
+                className="input input-bordered"
+                id="email"
+                type="email"
+                placeholder="Enter email"
+                value={inputValues.email}
+                name="email"
+                onChange={inputChangeHandler}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label" htmlFor="password">
+                <span className="label-text">Pasword</span>
+              </label>
+              <input
+                className="input input-bordered"
+                id="password"
+                type="password"
+                placeholder="Enter password"
+                value={inputValues.password}
+                name="password"
+                onChange={inputChangeHandler}
+              />
+            </div>
