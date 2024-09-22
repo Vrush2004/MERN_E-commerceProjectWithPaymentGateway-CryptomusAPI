09ee36f84c59b777cad9a1979e5d511e5f7b0934
Vrush2004 <vrushalichaube@gmail.com>
2024-09-22 19:45:29 +0530

    user list

--- client/src/pages/user-list/UserListScreen.jsx
+++ client/src/pages/user-list/UserListScreen.jsx
@@ -40,3 +40,87 @@ const UserListScreen = () => {
       dispatch(deleteUser(id));
     }
   };
+
+  return (
+    <Layout>
+      <Header className="justify-end">
+        <div className="flex items-center divide-x divide-gray-200 border-x border-b border-gray-200">
+          <Cart className="hidden lg:block p-6 text-palette-graniteGray" />
+          <UserProfileButton className="hidden lg:block p-6 text-palette-graniteGray" />
+        </div>
+      </Header>
+      <section className="flex flex-col p-5">
+        <h1 className="text-2xl mb-5 font-semibold">Users</h1>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Alert variant="error">{error}</Alert>
+        ) : (
+          <div className="-my-2 overflow-x-auto w-full">
+            <div className="inline-block min-w-full py-2 align-middle">
+              <div className="overflow-hidden border border-gray-200 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50 dark:bg-gray-800">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        <div className="flex items-center gap-x-3">
+                          <span>ID</span>
+                        </div>
+                      </th>
+
+                      <th
+                        scope="col"
+                        className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        <div className="flex items-center gap-x-3">
+                          <span>NAME</span>
+                        </div>
+                      </th>
+
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        <div className="flex items-center gap-x-3">
+                          <span>EMAIL</span>
+                        </div>
+                      </th>
+
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        ADMIN
+                      </th>
+
+                      <th scope="col" className="relative py-3.5 px-4"></th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {users.map((user) => (
+                      <tr key={user._id}>
+                        <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                          <div className="inline-flex items-center gap-x-3">
+                            <div className="flex items-center gap-x-2">
+                              <div>
+                                <h2 className="font-medium text-gray-800">
+                                  {user._id}
+                                </h2>
+                              </div>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                          <div className="inline-flex items-center gap-x-3">
+                            <div className="flex items-center gap-x-2">
+                              <div>
+                                <h2 className="font-medium text-gray-800">
+                                  {user.name}
+                                </h2>
+                              </div>
+                            </div>
+                          </div>
+                        </td>
